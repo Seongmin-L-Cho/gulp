2c8a22280a77f89ee0f11348201dbbe20763e8a2
DESKTOP-NJGRTB8\giner <Seongmin.L.Cho@gmail.com>
2022-04-03 23:54:38 +0900

    sass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,9 @@ import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
 import image from "gulp-image";
+import sass from "gulp-sass";
+
+sass.compiler = require("node-sass");
 
 const routes = {
   pug: {
@@ -14,6 +17,11 @@ const routes = {
   img: {
     src: "src/img/*",
     dest: "build/img"
+  },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/style.scss",
+    dest: "build/css"
   }
 };
 
@@ -25,11 +33,9 @@ export const pug = () =>
 
     const clean = () => del(["build/"]);
 
-    const webserver = () =>
-      gulp.src("build").pipe(ws({ livereload: true, open: true }));
+    const webserver = () => gulp.src("build").pipe(ws({ livereload: true }));
 
       const prepare = gulp.series([clean, img]);    // package.json 에 있는 거만 export 해주면 됨
-    const assets = gulp.series([pug]);
     
     const img = () =>
     gulp
@@ -37,11 +43,22 @@ export const pug = () =>
       .pipe(image())
       .pipe(gulp.dest(routes.img.dest));
 
+
+      const styles = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest(routes.scss.dest));
+
+
     const watch = () => {
       gulp.watch(routes.pug.watch, pug);
       gulp.watch(routes.img.src, img);
+      gulp.watch(routes.scss.watch, styles);
+
     };
     
+    const assets = gulp.series([pug, styles]);
 
     
 
